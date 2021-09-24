6639f5ca0112660c943d56f580496005e040e8ad
Sam Sepiol <micobc@protonmail.com>
2021-09-23 22:01:14 -0700

    add navigation flow

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
 //alert("Hello World");
 
 /*just commented it out for refresh rate. - Melanie*/
+
+function showMessage(id){
+    switch(id){
+        case "compliance":
+            hide("flu")
+            break;
+        case "success":
+            hide("compliance");
+            break;
+        case "failure":
+            hide("flu");
+            hide("compliance");
+            break;
+    }
+    show(id);
+}
+
+function resetApp(){
+    hide("success");
+    hide("failure");
+    hide("compliance");
+    show("flu");
+}
+
+function hide(id) {
+    document.getElementById(id).style.display = "none";
+}
+
+function show(id) {
+    document.getElementById(id).style.display = "block";
+}
